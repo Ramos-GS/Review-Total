e2fa50faa805cdec7d4496f591fce2ff9d51fda1
Erick Cassoli <erick.mcassoli@gmail.com>
2024-06-03 19:37:03 -0300

    feat: create a views add views and render to project

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Movie, Series, Game, Review
 
-# Create your views here.
+def index(request):
+    return render(request, 'home_screen.html')
+
+def movie_list(request):
+    movies = Movie.objects.all()
+    return render(request, 'movie_list.html', {'movies': movies})
+
+def series_list(request):
+    series = Series.objects.all()
+    return render(request, 'series_list.html', {'series': series})
+
+def game_list(request):
+    games = Game.objects.all()
+    return render(request, 'game_list.html', {'games': games})
+
+def review_detail(request, review_id):
+    review = get_object_or_404(Review, id=review_id)
+    return render(request, 'review_detail.html', {'review': review})
